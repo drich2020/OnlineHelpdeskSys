c21aff8467b7e7133f0f23c57d612de64869fd52
drich2020 <60243531+drich2020@users.noreply.github.com>
2021-12-28 11:54:11 -0500

    Remote host update

--- config.php
+++ config.php
@@ -1,6 +1,12 @@
 <?php
-    $host = "localhost";
+    /*$host = "localhost";
     $username = "root";
     $password = "";
-    $database = "helpdesk_system";
+    $database = "helpdesk_system";*/
+
+    $host = "bkt7ykqu8ne9eusysyib-mysql.services.clever-cloud.com";
+    $username = "urb8iljhqtw8cwox";
+    $password = "mg0odCRaYObDeNJGgbPQ";
+    $database = "bkt7ykqu8ne9eusysyib";
+
 ?>
